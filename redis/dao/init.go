c8d6c017d582c5b600c4d944bb2cd35ba4889b3e
beewind <85089712+beewind@users.noreply.github.com>
2022-09-05 16:35:48 +0800

    Update init.go

--- redis/dao/init.go
+++ redis/dao/init.go
@@ -8,7 +8,7 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-const DSN string = "demo:demo@tcp(123.56.17.118:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
+const DSN string = "demo:demo@tcp(192.168.1.102:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
 
 var db *gorm.DB
 
